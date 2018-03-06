af3308b91d2adb36e0d5cc2e2a743f74424b019f
DJ Sitte <DJS@nbf.com>
2018-03-06 15:23:58 -0600

    Restarting project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { CurrentProjectsComponentComponent } from './current-projects-component/current-projects-component.component';
+import { EducationComponentComponent } from './education-component/education-component.component';
+import { TopNavComponentComponent } from './top-nav-component/top-nav-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    CurrentProjectsComponentComponent,
+    EducationComponentComponent,
+    TopNavComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/current-projects-component/current-projects-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-projects-component',
+  templateUrl: './current-projects-component.component.html',
+  styleUrls: ['./current-projects-component.component.css']
+})
+export class CurrentProjectsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
