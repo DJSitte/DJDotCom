72f2b761dcbd6e28d57fc723464716ec65d75192
DJ Sitte <DJS@nbf.com>
2018-03-07 12:06:39 -0600

    Little work on the page. Got the toolbar back up.

--- src/app/current-projects/current-projects.component.ts
+++ src/app/current-projects/current-projects.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-current-projects',
   templateUrl: './current-projects.component.html',
-  styleUrls: ['./current-projects.component.scss']
+  styleUrls: ['./current-projects.component.scss'],
+  host: {class: 'component-wrapper'}
 })
 export class CurrentProjectsComponent implements OnInit {
 
--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
+  host: {class: 'component-wrapper'}
 })
 export class EducationComponent implements OnInit {
 
